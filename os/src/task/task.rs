f64d3b90f5ad9143eb2fb576bd15ea325473c416
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2023-08-05 21:14:38 +0800

    simplified spawn

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -214,53 +214,6 @@ impl TaskControlBlock {
         // ---- release parent PCB
     }
 
-    /// spawn a process, fork without copy parent address
-    pub fn spawn(self: &Arc<Self>, elf_data: &[u8]) -> Arc<Self> {
-        // fork from elf
-        // ---- access parent PCB exclusively
-        let mut parent_inner = self.inner_exclusive_access();
-        // copy user space(include trap context)
-        let (memory_set, user_sp_base, _entry_point) = MemorySet::from_elf(elf_data);
-        let trap_cx_ppn = memory_set
-            .translate(VirtAddr::from(TRAP_CONTEXT_BASE).into())
-            .unwrap()
-            .ppn();
-        // alloc a pid and a kernel stack in kernel space
-        let pid_handle = pid_alloc();
-        let kernel_stack = kstack_alloc();
-        let kernel_stack_top = kernel_stack.get_top();
-        let task_control_block = Arc::new(TaskControlBlock {
-            pid: pid_handle,
-            kernel_stack,
-            inner: unsafe {
-                UPSafeCell::new(TaskControlBlockInner {
-                    trap_cx_ppn,
-                    base_size: user_sp_base,
-                    task_cx: TaskContext::goto_trap_return(kernel_stack_top),
-                    task_status: TaskStatus::Ready,
-                    memory_set,
-                    parent: Some(Arc::downgrade(self)),
-                    children: Vec::new(),
-                    exit_code: 0,
-                    heap_bottom: user_sp_base,
-                    program_brk: user_sp_base,
-                    syscall_times: [0; MAX_SYSCALL_NUM],
-                    start_time: 0,
-                })
-            },
-        });
-        // add child
-        parent_inner.children.push(task_control_block.clone());
-        // modify kernel_sp in trap_cx
-        // **** access child PCB exclusively
-        let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
-        trap_cx.kernel_sp = kernel_stack_top;
-        // return
-        task_control_block
-        // **** release child PCB
-        // ---- release parent PCB
-    }
-
     /// get pid of process
     pub fn getpid(&self) -> usize {
         self.pid.0
